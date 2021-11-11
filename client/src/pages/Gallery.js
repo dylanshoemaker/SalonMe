23a6f37cfae9198656063031fa447ff7e82b02c7
Trent Roesbery <xtrent777x@gmail.com>
2021-11-10 21:03:19 -0700

    gallery photos in gallery page

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -5,7 +5,14 @@ const Gallery = () => {
     return (
       <div className="container">
         
-        <h2>Test</h2>
+        <h2>GALLERY</h2>
+        
+        <img src="/images/color-image1.jpg" alt="red color" width="500" height="600"></img>
+        <img src="/images/color-image2.jpg" alt="red color 2" width="500" height="600" ></img>
+        <img src="/images/color-image3.jpg" alt="kid cut and style" width="500" height="600"></img>
+        <img src="/images/color-image4.jpg" alt="Ombre" width="500" height="600"></img>
+        <img src="/images/color-image5.jpg" alt="green and blue" width="500" height="600"></img>
+        <img src="/images/color-image6.jpg" alt="pink orange and purple" width="500" height="600"></img>
         
       </div>
     );
