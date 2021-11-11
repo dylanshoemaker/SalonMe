ba19085e07efb0644f3ab4db820bcc18ff5fcc99
Matt <matt.white.624@gmail.com>
2021-11-10 19:34:20 -0700

    npm stuffs

---
+++ client/build/precache-manifest.d6bbadf7e2d74dc5a1a6bde758daa46e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b846e43c1a19d3b2449e18282e00d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9a429e436634edb069",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "bdcb6363b90ef56e14fb",
+    "url": "/static/js/2.50707ecd.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.50707ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9a429e436634edb069",
+    "url": "/static/js/main.48e3e8d5.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
