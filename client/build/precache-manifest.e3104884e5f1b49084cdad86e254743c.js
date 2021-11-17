8a3e1ba39c90ee6cdadc4a19e3a37ef3e493c154
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-16 18:39:22 -0800

    make react app a pwa

---
+++ client/build/precache-manifest.e3104884e5f1b49084cdad86e254743c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e36b376f82def47049de88d794c517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a90d060aafe00410d2",
+    "url": "/static/css/2.eccacc77.chunk.css"
+  },
+  {
+    "revision": "fc3edc4ca43cd836fc83",
+    "url": "/static/css/main.66476a9d.chunk.css"
+  },
+  {
+    "revision": "89a90d060aafe00410d2",
+    "url": "/static/js/2.0c92391b.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.0c92391b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc3edc4ca43cd836fc83",
+    "url": "/static/js/main.889f65b4.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
